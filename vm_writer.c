ceeb15dccdc2f92e194f8e420734b5e5668869aa
tombour <ridgerouzier@gmail.com>
2025-04-15 21:18:46 -0400

    passes all tests for chapter 7

--- vm_writer.c
+++ vm_writer.c
@@ -2,12 +2,60 @@
 #include"vm_writer.h"
 #include<stdlib.h>
 
+#define EQ_OP "D=M-D\n" \
+	"@cond.%lu\n"\
+	"D;JEQ\n"\
+	"@SP\n"\
+	"A=M-1\n"\
+	"M=0\n"\
+	"@3\n"\
+	"D=A\n"\
+	"@cond.%lu\n"\
+	"A=D+A\n"\
+	"0;JMP\n"\
+	"(cond.%lu)\n"\
+	"@SP\n"\
+	"A=M-1\n"\
+	"M=-1\n"
+
+#define LT_OP "D=M-D\n" \
+	"@cond.%lu\n"\
+	"D;JLT\n"\
+	"@SP\n"\
+	"A=M-1\n"\
+	"M=0\n"\
+	"@3\n"\
+	"D=A\n"\
+	"@cond.%lu\n"\
+	"A=D+A\n"\
+	"0;JMP\n"\
+	"(cond.%lu)\n"\
+	"@SP\n"\
+	"A=M-1\n"\
+	"M=-1\n"
+
+#define GT_OP "D=M-D\n" \
+	"@cond.%lu\n"\
+	"D;JGT\n"\
+	"@SP\n"\
+	"A=M-1\n"\
+	"M=0\n"\
+	"@3\n"\
+	"D=A\n"\
+	"@cond.%lu\n"\
+	"A=D+A\n"\
+	"0;JMP\n"\
+	"(cond.%lu)\n"\
+	"@SP\n"\
+	"A=M-1\n"\
+	"M=-1\n"
+static uint64 cond_counter = 0;
 static char segment_list[][5]= {
 	XSTR(STATIC_BASE),XSTR(LOCAL_BASE),XSTR(ARG_BASE),XSTR(THIS_BASE),
 	XSTR(THAT_BASE),XSTR(THIS_BASE),XSTR(TEMP_BASE),XSTR(STACK_BASE)
 	};
-static char op_list[][6] = {
-	"M=D+M","M=M-D","M","M","M","M=D&M",
+static char op_list[][256] = {
+	"M=D+M","M=M-D",EQ_OP,GT_OP,LT_OP,"M=D&M",
 	"M=D|M","M=!M","M=-M"
 };
 VM_Writer *vm_create_writer(char *file)
@@ -66,6 +114,7 @@ void vm_write_push(VM_Writer *writer, VM_Segment segment, String_Snap index)
 		fprintf(writer->output,"M=M+1\n");
 }
 
+// optimize this
 void vm_write_arithmetic(VM_Writer *writer,VM_Op command)
 {
 	char *op = op_list[command];
@@ -75,9 +124,6 @@ void vm_write_arithmetic(VM_Writer *writer,VM_Op command)
 		case VM_SUB:
 		case VM_AND:
 		case VM_OR:
-		case VM_LT:
-		case VM_GT:
-		case VM_EQ:
 			fprintf(writer->output,"@SP\n");
 			fprintf(writer->output,"M=M-1\n");
 			fprintf(writer->output,"A=M\n");
@@ -87,9 +133,35 @@ void vm_write_arithmetic(VM_Writer *writer,VM_Op command)
 		case VM_NOT:
 		case VM_NEG:
 			fprintf(writer->output,"@SP\n");
+			fprintf(writer->output,"A=M-1\n");
+			break;
+		case VM_LT:
+			fprintf(writer->output,"@SP\n");
 			fprintf(writer->output,"M=M-1\n");
 			fprintf(writer->output,"A=M\n");
-			break;
+			fprintf(writer->output,"D=M\n");
+			fprintf(writer->output,"A=A-1\n");
+			fprintf(writer->output,LT_OP,cond_counter,cond_counter,cond_counter);
+			++cond_counter;
+			return;
+		case VM_GT:
+			fprintf(writer->output,"@SP\n");
+			fprintf(writer->output,"M=M-1\n");
+			fprintf(writer->output,"A=M\n");
+			fprintf(writer->output,"D=M\n");
+			fprintf(writer->output,"A=A-1\n");
+			fprintf(writer->output,GT_OP,cond_counter,cond_counter,cond_counter);
+			++cond_counter;
+			return;
+		case VM_EQ:
+			fprintf(writer->output,"@SP\n");
+			fprintf(writer->output,"M=M-1\n");
+			fprintf(writer->output,"A=M\n");
+			fprintf(writer->output,"D=M\n");
+			fprintf(writer->output,"A=A-1\n");
+			fprintf(writer->output,EQ_OP,cond_counter,cond_counter,cond_counter);
+			++cond_counter;
+			return;
 		}
 	// write the actual operation
 		fprintf(writer->output,"%s\n",op);
